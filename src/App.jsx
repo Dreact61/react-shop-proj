b6ce077749625b914bf615e0a816a67f6bcb9dcd
nikita k. <dreact61@gmail.com>
2026-05-20 20:59:42 +0300

    1.1.0  почти полностью завершенная главная страница

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,43 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import './App.css'
 import { storeValues } from './components/shopStore'
 
 function App() {
-  const { products, cart, loading, error, fetchProducts } = storeValues()
+  const { products, cart, loading, error, topProducts, balance, handleBalance, fetchProducts, sortProductsByRating, addToCart, removeFromCart, clearCart, buyCartItem, extractOneCartItem } = storeValues()
 
   useEffect(() => {
     fetchProducts()
   }, [fetchProducts])
 
+  useEffect(() => {
+    if (products.length > 0) sortProductsByRating()
+  }, [sortProductsByRating, products.length])
+
   return (
     <div className='body'>
       <header>
         <h1>Shop</h1>
-        <div className='header-element'>
+        <p>Balance: ${balance.toFixed(2)}<input type="number" placeholder='Change your balance here.' id="balance-input" onBlur={(e) => handleBalance(Number(e.target.value))} defaultValue={balance.toFixed(2)}/></p>
+
+        <nav className='header-element'>
           <p><a href="#">Sign in</a></p>
           <p><a href="#">About us</a></p>
           <p><a href="#">FAQ</a></p>
-        </div>
+        </nav>
       </header>
 
-      <nav>
-        <h3>Trended offers:</h3>
+      <section className='rating-section'>
+        <h3>Top 5 positions:</h3>
         {loading ? <p className="loading-msg">Loading...</p> : ''}
-        <ul>
           {error === null ? 
-            products.map(product => (
-              <li></li>
+            topProducts.slice(0,5).map(product => (
+                <div className="top-menu" key={product.id}>
+                  <b>{product.title}</b>
+                  <p>Rating: {product.rating}</p>
+                </div>
             )) : <p className='error-msg'>Error :(</p>
           }
-        </ul>
-      </nav>
+      </section>
 
       <main>
         {loading ? <p className="loading-msg">Loading...</p> : ''}
@@ -41,11 +48,30 @@ function App() {
             <b>{product.title}</b>
             <p>{product.description}</p>
             <i>${product.price}</i>
-            <button type="button">Add to cart</button>
+            <button type="button" className='add-to-cart-btn' onClick={() => addToCart(product)}>Add to cart</button>
           </div>
         )) : <p className='error-msg'>Error :(</p>
         }
       </main>
+
+      <section className='cart-section'>
+        <h3>Your cart</h3>
+        {cart && cart.length > 0 ? 
+        cart.map(item => (
+          <div key={item.id} className='cart-item'>
+            <b>{item.title}</b>
+            <p>Quantity: {item.quantity}</p>
+            <p>Summary: ${Number(item.price) * Number(item.quantity)}</p>
+            <button type='button' className='add-to-cart-btn' style={{width:"30%"}} onClick={() => buyCartItem(item)}>Buy</button>
+            <button type="button" className='add-to-cart-btn' style={{width:"30%"}} onClick={() => extractOneCartItem(item.id)}>Remove one</button>
+          </div>
+        )) : <p>No products found in your cart.</p>}
+        <button type="button" className='add-to-cart-btn' onClick={clearCart}>Clear cart</button>
+      </section>
+      
+      <footer>
+        <button type="button">Sign in</button>
+      </footer>
     </div>
   )
 }
--- src/components/shopStore.ts
+++ src/components/shopStore.ts
@@ -8,7 +8,8 @@ export type Product = {
     price: number,
     category: string,
     description: string,
-    image: string 
+    image: string,
+    rating: number
 }
 
 export interface CartItem extends Product {
@@ -17,16 +18,22 @@ export interface CartItem extends Product {
 
 export type ValuesType = {
     products: Product[],
+    topProducts: Product[],
     cart: CartItem[],
+    balance: number,
     loading: boolean,
     error: string | null
 }
 
 export type OperationsType = {
-    fetchProducts: () => Promise<void>,
+    fetchProducts: () => Promise<void>, //вызов api
+    sortProductsByRating: () => unknown,
     addToCart: (product: Product) => void,
     removeFromCart: (id: number) => void,
-    clearCart: () => void
+    clearCart: () => void,
+    extractOneCartItem: (productId: number) => void,
+    buyCartItem: (product: CartItem) => void,
+    handleBalance: (num: number) => void //функция выдачи денег, т.к. кошелек сайта фальшивый
 }
 
 export type StoreType = OperationsType & ValuesType
@@ -35,23 +42,26 @@ export const storeValues = create<StoreType>()(
     persist(
         (set, get) => ({
             products: [],
+            topProducts: [],
             cart: [],
+            balance: 0,
             loading: false,
             error: null,
 
             fetchProducts: async () => {
                 set({ loading: true, error: null });
                 try {
-                    const response = await axios.get<{ products: any[] }>('https://dummyjson.com/products');
+                    const response = await axios.get<{ products: any[] }>('https://dummyjson.com/products')
                     const formattedProducts: Product[] = response.data.products.map(item => ({
                         id: item.id,
                         title: item.title,
                         price: item.price,
                         category: item.category,
                         description: item.description,
-                        image: item.thumbnail 
+                        image: item.thumbnail,
+                        rating: item.rating
                     }));
-                    set({ products: formattedProducts, loading: false });
+                    set({ products: formattedProducts, loading: false })
                 } catch (error: any) {
                     set({ 
                         error: error.message || 'Error while fetching data', 
@@ -59,16 +69,20 @@ export const storeValues = create<StoreType>()(
                     });
                 }
             },
+            sortProductsByRating: () => {
+                const { products, topProducts } = get()
+                set({topProducts: products.sort((a,b) => b.rating - a.rating)})
+            },
 
             addToCart: (product) => {
-                const { cart } = get();
-                const exists = cart.find(item => item.id === product.id);   
+                const { cart } = get()
+                const exists = cart.find(item => item.id === product.id);  
                 if (exists) {
                     set({
                         cart: cart.map(item => item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item)
                     });
                 } else {
-                    set({ cart: [...cart, { ...product, quantity: 1 }] });
+                    set({ cart: [...cart, { ...product, quantity: 1 }] })
                 }
             },
 
@@ -79,11 +93,50 @@ export const storeValues = create<StoreType>()(
                 });
             },
 
-            clearCart: () => set({ cart: [] })
+            clearCart: () => {
+                const {cart} = get()
+                if (cart.length) {
+                    set({ cart: [] })
+                    alert('Cart cleared.')
+                } else {
+                    return
+                }
+            },
+
+            extractOneCartItem: (productId) => {
+                const {cart} = get()
+                const target = cart.find(item => item.id === productId)
+                if (!target) return
+
+                if (target.quantity === 1) {
+                    set({cart: cart.filter(item => item.id !== productId)})
+                } else {
+                    set({cart: cart.map(item => item.id === productId 
+                        ? {...item, quantity: item.quantity - 1} 
+                        : item
+                    )})
+                }
+            },
+
+            buyCartItem: (product) => {
+                const {cart, balance} = get()
+                const summary = product.quantity * product.price
+                if (balance >= summary) {
+                    set({
+                        balance: balance - summary,
+                        cart: cart.filter(item => item.id !== product.id)
+                    })
+                    alert(`${product.title} was bought successfully! balance was changed to ${(balance - summary).toFixed(Number(2))}`)
+                } else {
+                    return
+                }
+            },
+
+            handleBalance: (num) => set({balance: num})
         }),
         { 
             name: 'shop-data',
-            partialize: (state) => ({ cart: state.cart })
+            partialize: (state) => ({ cart: state.cart, balance: state.balance })
         }
     )
 );
